5d5506dc72c0d50c4450f2ed31ef8fe6cfbeb5ac
kelvinachiando <kelvinostin22gmail.com>
2022-07-16 19:44:10 +0300

    Assignment Completed

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,9 +1,25 @@
 import React from "react";
 
+// parent component
+function App() {
+  const title = "Mad Max";
+  const posterURL =
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTn1OTYGz2GDC1XjA9tirh_1Rd571yE5UFIYsmZp4nACMd7CCHM";
+  const genresArr = ["Action", "Adventure", "Science Fiction", "Thriller"];
+
+  return (
+    <div className="App">
+      {/* passing down props from the parent component */}
+      <MovieCard title={title} genres={genresArr} />
+    </div>
+  );
+}
+
+// child component
 function MovieCard({
   title,
-  posterSrc = "https://m.media-amazon.com/images/M/MV5BOTJjNzczMTUtNzc5MC00ODk0LWEwYjgtNzdiOTEyZmQxNzhmXkEyXkFqcGdeQXVyNzMzMjU5NDY@._V1_UY268_CR1,0,182,268_AL_.jpg",
   genres,
+  posterSrc = "https://m.media-amazon.com/images/M/MV5BOTJjNzczMTUtNzc5MC00ODk0LWEwYjgtNzdiOTEyZmQxNzhmXkEyXkFqcGdeQXVyNzMzMjU5NDY@._V1_UY268_CR1,0,182,268_AL_.jpg",
 }) {
   return (
     <div className="movie-card">
